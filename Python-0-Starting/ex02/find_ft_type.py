fedc50091559d000676fbf03a75a8c114431b2b3
mhoyer <mathieuhoyer@gmail.com>
2024-03-13 21:26:42 +0100

    maj

--- Python-0-Starting/ex02/find_ft_type.py
+++ Python-0-Starting/ex02/find_ft_type.py
@@ -1,3 +1,18 @@
 def all_thing_is_obj(object: any) -> int:
-	print(object, ":", type(object))
+	typeNames = {
+		list: "List",
+		tuple: "Tuple",
+		set: "Set",
+		dict: "Dict",
+		str: "str"
+	}
+	objectType = type(object)
+	typeName = typeNames.get(objectType, "Type not found")
+ 
+	if typeName == "str":
+		print(f'{object} is in the kitchen : {objectType}')
+	elif typeName != "Type not found":
+		print(f'{typeName} : {objectType}')
+	else:
+		print(typeName)
 	return 42
