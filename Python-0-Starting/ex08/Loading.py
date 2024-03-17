151822118537ea6011846c8db31d5a5092a54bd1
mhoyer <mathieuhoyer@gmail.com>
2024-03-17 12:57:12 +0100

    maj

--- Python-0-Starting/ex08/Loading.py
+++ Python-0-Starting/ex08/Loading.py
@@ -1,2 +1,7 @@
-def ft_tqdm(lst: range) -> None:
-    pass
+def ft_tqdm(lst: range):
+    total = len(lst)
+    for i, elem in enumerate(lst):
+        percent = int((i + 1) * 100 / total)
+        print(f'\r{percent}%|[' + percent * '=' +
+              (100 - percent) * ' ' + f']| {i + 1}/{total}', end='')
+        yield elem
