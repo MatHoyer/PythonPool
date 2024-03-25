9640e173dfe47bd2680278baae575057de63b48b
mhoyer <mathieuhoyer@gmail.com>
2024-03-25 15:44:22 +0100

    maj

--- Python-1-Array/ex01/array2D.py
+++ Python-1-Array/ex01/array2D.py
@@ -1,7 +1,16 @@
+import numpy as np
+
+
 def slice_me(family: list, start: int, end: int) -> list:
     try:
-        int(start), int(end)
+        start = abs(int(start))
+        end = abs(int(end))
+        array = np.array(family)
         
+        print(f'My shape is : {array.shape}')
+        sliced_array = array[start:end]
+        print(f'My new shape is : {sliced_array.shape}')
+        return(sliced_array)
     except ValueError as e:
         print(ValueError.__name__, ':', e)
         exit(1)
