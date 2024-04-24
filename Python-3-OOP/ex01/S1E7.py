17811a9a1f8d878d17fbd97b9c8757839692dd0a
mhoyer <mathieuhoyer@gmail.com>
2024-04-24 13:29:26 +0200

    maj

---
+++ Python-3-OOP/ex01/S1E7.py
@@ -0,0 +1,35 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    '''Baratheon familly'''
+    def __init__(self, name: str, is_alive: bool = True):
+        '''Initialize the Baratheon familly member'''
+        super().__init__(name, eyes='brown', hairs='dark', is_alive=is_alive)
+
+    def __str__(self) -> str:
+        return f"Vector: ('{self.__class__.__name__}', '{self.eyes}', '{self.hairs}')"
+    
+    def __repr__(self) -> str:
+        return f"Vector: ('{self.__class__.__name__}', '{self.eyes}', '{self.hairs}')"
+
+
+
+class Lannister(Character):
+    '''Lannister familly'''
+    def __init__(self, name: str, is_alive: bool = True):
+        '''Initialize the Lannister familly member'''
+        super().__init__(name, eyes='blue', hairs='light', is_alive=is_alive)
+
+    def __str__(self) -> str:
+        return f"Vector: ('{self.__class__.__name__}', '{self.eyes}', '{self.hairs}')"
+    
+    def __repr__(self) -> str:
+        return f"Vector: ('{self.__class__.__name__}', '{self.eyes}', '{self.hairs}')"
+
+
+    @staticmethod
+    def create_lannister(name: str, is_alive: bool = True):
+        '''Create a Lannister member'''
+        return Lannister(name, is_alive)
+    
--- Python-3-OOP/ex01/S1E9.py
+++ Python-3-OOP/ex01/S1E9.py
@@ -4,15 +4,18 @@ from abc import ABC, abstractmethod
 class Character(ABC):
     '''Abstract class for characters'''
     @abstractmethod
-    def __init__(self, name: str, is_alive: bool = True):
+    def __init__(self, name: str, eyes: str, hairs: str, is_alive: bool = True):
         '''Initialize the character'''
         self.first_name = name
         self.is_alive = is_alive
+        self.family_name = self.__class__.__name__
+        self.eyes = eyes
+        self.hairs = hairs
 
     def die(self):
         '''Kill the character'''
         self.is_alive = False
-    
+
 
 class Stark(Character):
     '''Stark familly inherits from character class'''
