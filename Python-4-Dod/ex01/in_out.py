ea7ec5349c142303c43a113355405b5aab63b121
mhoyer <mathieuhoyer@gmail.com>
2024-04-26 20:21:22 +0200

    maj

---
+++ Python-4-Dod/ex01/in_out.py
@@ -0,0 +1,15 @@
+def square(x: int | float) -> int | float:
+    return x**2
+
+
+def pow(x: int | float) -> int | float:
+    return x**x
+
+
+def outer(x: int | float, function) -> object:
+    mem = x
+    def inner() -> float:
+        nonlocal mem
+        mem = function(mem)
+        return mem
+    return inner
