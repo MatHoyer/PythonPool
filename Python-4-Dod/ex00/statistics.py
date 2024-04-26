cda64d981121300da214024397384c9af81e320c
mhoyer <mathieuhoyer@gmail.com>
2024-04-26 19:31:33 +0200

    maj

--- Python-4-Dod/ex00/statistics.py
+++ Python-4-Dod/ex00/statistics.py
@@ -1,27 +1,29 @@
 def mean(lst: list):
-    print(sum(lst) / len(lst))
+    return (sum(lst) / len(lst))
 
 
 def median(lst: list):
-    print(lst[len(lst) // 2])
+    return (lst[len(lst) // 2])
 
 
 def quartile(lst: list):
     first = len(lst) // 4
     last = len(lst) - first - 1
-    print([float(lst[first]), float(lst[last])])
+    return ([float(lst[first]), float(lst[last])])
+
+
+def calcVariance(lst: list):
+    mean = sum(lst) / len(lst)
+    ecaAll = sum((mean - el)**2 for el in lst)
+    return ecaAll / len(lst)
 
 
 def std(lst: list):
-    mean = sum(lst) / len(lst)
-    ecaAll = sum((mean - el)**2 for el in lst)
-    print((ecaAll / len(lst))**(0.5))
+    return ((calcVariance(lst))**(0.5))
 
 
 def var(lst: list):
-    mean = sum(lst) / len(lst)
-    ecaAll = sum((mean - el)**2 for el in lst)
-    print(ecaAll / len(lst))
+    return (calcVariance(lst))
 
 
 def ft_statistics(*args, **kwargs):
@@ -43,6 +45,6 @@ def ft_statistics(*args, **kwargs):
         if not args:
             print("ERROR")
             continue
-        func(lst)
+        print(func(lst))
 
     return
