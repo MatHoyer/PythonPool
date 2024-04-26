88d2ce3c094f3479764d3fd27eb5d577bb3a89b8
mhoyer <mathieuhoyer@gmail.com>
2024-04-26 21:11:53 +0200

    maj

---
+++ Python-4-Dod/ex02/callLimit.py
@@ -0,0 +1,12 @@
+def callLimit(limit: int):
+    count = 0
+    def callLimiter(function):
+        def limit_function(*args, **kwds):
+            nonlocal count
+            if count >= limit:
+                print('Error:', function, 'call too many times')
+                return
+            count += 1
+            return function(*args, **kwds)
+        return limit_function
+    return callLimiter
